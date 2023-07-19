ce820409521f01c68439aef786561547d173edae
Diego Haro Vivanco <diego.harovivanco@gmail.com>
2023-07-18 23:58:36 -0300

    maquetacion basica

---
+++ config/config.php
@@ -0,0 +1,8 @@
+<?php
+
+    //Rutas del proyecto
+    //Para el admin
+    define('RUTA_ADMIN', 'http://localhost/PROYPHP/blog_php/admin/');
+
+    //Para el front
+    define('RUTA_FRONT', 'http://localhost/PROYPHP/blog_php/');
